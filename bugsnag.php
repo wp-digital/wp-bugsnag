c81cb13d6f73595daa4c908b0aac6e3f115bc49a
Aleksandr Strikha <alex@pingbull.no>
2016-11-11 11:03:48 +0100

    Add front-end errors tracking

--- bugsnag.php
+++ bugsnag.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Bugsnag
 Description: WordPress plugin for tacking errors to Bugsnag
-Version: 1.1
+Version: 1.2
 Plugin URI: https://github.com/shtrihstr/wp-bugsnag
 Author: Oleksandr Strikha
 Author URI: https://github.com/shtrihstr
@@ -17,11 +17,31 @@ if ( defined( 'BUGSNAG_API_KEY' ) ) {
     $bugsnag = new Bugsnag_Client( BUGSNAG_API_KEY );
     $bugsnag->setErrorReportingLevel( E_ERROR | E_PARSE );
 
-    set_error_handler( array( $bugsnag, 'errorHandler' ) );
-    set_exception_handler( array( $bugsnag, 'exceptionHandler' ) );
+    set_error_handler( [ $bugsnag, 'errorHandler' ] );
+    set_exception_handler( [ $bugsnag, 'exceptionHandler' ] );
 
     add_action( 'bugsnag_notify', function( $name, $message ) use ( $bugsnag ) {
         $bugsnag->notifyError( $name, $message );
     }, 1, 2 );
+
+}
+
+
+if ( defined( 'BUGSNAG_FRONTEND_API_KEY' ) && ! is_admin() ) {
+
+    add_action( 'wp_enqueue_scripts', function() {
+
+        wp_enqueue_script( 'bugsnag', '//d2wy8f7a9ursnm.cloudfront.net/bugsnag-3.min.js' );
+
+    }, -1 );
+
+    add_filter( 'script_loader_tag', function( $tag, $handle ) {
+
+        if( 'bugsnag' == $handle ) {
+            $tag = str_replace( 'src=', sprintf( "data-apikey='%s' src=", esc_attr( BUGSNAG_FRONTEND_API_KEY ) ), $tag );
+        }
+
+        return $tag;
+    }, 5, 2 );
 
 }
